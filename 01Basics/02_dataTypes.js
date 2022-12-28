e97a67bc97d78c741a97d6d2a27deb957706c586
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2022-12-28 10:49:38 +0000

    learnt datatypes

---
+++ 01Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodeJs, not browser
+
+// https://tc39.es/ecma262/#sec-intro -> official document
+
+let name = "Ankit";
+let age = 23;
+let isLoggedIn = false;
+
+/*
+DATA_TYPES:
+ Primitive-
+    1)number => 2 to power 53
+    2)bigint
+    3)string => "",''
+    4)boolean => true/false
+    5)null => standalone value
+    6)undefined =>
+    7)symbol => unique
+ Non-Primitive
+    1)object
+*/
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
