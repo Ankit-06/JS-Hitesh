dd37082fed8d8a2e0f143499208af09aaaa35df3
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2023-04-05 07:50:36 +0000

    Why string to number conversion is confusing

--- 01Basics/03_conversionOperation.js
+++ 01Basics/03_conversionOperation.js
@@ -21,10 +21,50 @@ let isLoggedIn = 0;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "Ankit" => true
 // undefined => false
 // null => false
+
+//****************** Operations ***********************
+
+let value = 3;
+let negValue = -value;
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Ankit";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+// console.log(1 + "2" + 2); //122
+// console.log(3 + ((4 * 5) % 3));
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+// console.log(num1); //4
+// console.log(num2); //4
+// console.log(num3); //4
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
