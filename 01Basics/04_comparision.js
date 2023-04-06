bdfbfa6e6de4dd3b71d520c4b93760b316ecb57a
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2023-04-06 09:28:33 +0000

    comparision

--- 01Basics/04_comparision.js
+++ 01Basics/04_comparision.js
@@ -3,7 +3,7 @@
 // console.log(2<1);
 // console.log(2==1);
 // console.log(2!=1);
-com
+
 // console.log("2" > 1); //true
 // console.log("02" > 1); //true
 
@@ -16,3 +16,11 @@ Reason is that an equality check == and comparisons > < >= <= works differently.
 Comparions convert null to a number, treating it as 0.
 That's why null>=0 is true and null>0 is false
 */
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+
+// === strict check
+
+console.log("2" === 2); //false
+
