51b224f5df7b600862106bf13253e08e0fd58a1d
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2023-05-05 19:58:04 +0000

    objects keys,vakues and hasOwnProperty

---
+++ 02Basics/04_objects.js
@@ -0,0 +1,40 @@
+// const tinderUser = new Object(); //singleton object
+const tinderUser = {}; //non-singleton
+
+tinderUser.id = "123abc";
+tinderUser.name = "Ankit";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "ankit@gmail.com",
+  userName: {
+    fullName: {
+      firstName: { name: "Ankit" },
+      lastName: {},
+    },
+  },
+};
+
+// console.log(regularUser.userName.fullName.firstName.name);
+// console.log(regularUser.userName.fullName.lastName?.name?.k);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+// const obj3 = { obj1, obj2 };
+
+const obj3 = Object.assign({}, obj1, obj2);
+const obj = { ...obj1, ...obj2 }; // using spread operator
+
+// console.log(obj3);
+// console.log(obj);
+// console.log(obj3 === obj);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
