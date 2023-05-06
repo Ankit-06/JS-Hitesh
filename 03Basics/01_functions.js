7cb7556c5c16c9b8e30b54f1ea2a991a6b244ce6
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2023-05-06 08:22:19 +0000

    started functions

---
+++ 03Basics/01_functions.js
@@ -0,0 +1,16 @@
+function sayMyName() {
+  console.log("A");
+  console.log("N");
+  console.log("K");
+  console.log("I");
+  console.log("T");
+}
+
+// sayMyName();
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+// console.log(sum(5, "a"));
+console.log(sum(5, null));
