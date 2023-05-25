4abfaec263a3069e753d2a00ed9221f3dcf04e42
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2023-05-25 09:57:18 +0000

    iteration

---
+++ 05Iterations/01_one.js
@@ -0,0 +1,26 @@
+// for loop
+// iteration variable init, condition check, increment/decrement
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   for (let j = 0; j <= 10; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
+
+// let myArray = ["flash", "batman", "superman"];
+// console.log(myArray.length);
+
+// for (let i = 0; i < myArray.length; i++) {
+//   console.log(myArray[i]);
+// }
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+  if (index === 5) break;
+  console.log(`Value of i is ${index}`);
+}
