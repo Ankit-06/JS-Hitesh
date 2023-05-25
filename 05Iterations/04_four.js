32dc2459d8f303d6be02c74228a88813ab8a1be6
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2023-05-25 18:08:47 +0000

    for in loop

---
+++ 05Iterations/04_four.js
@@ -0,0 +1,27 @@
+// const myObject = {
+//   js: "Javascript",
+//   cpp: "C++",
+//   rb: "Ruby",
+//   swift: "swift by apple",
+// };
+
+// for (const key in myObject) {
+//   console.log(`${key} shortform is of ${myObject[key]}`);
+// }
+
+// const arr = ["js", "rb", "java", "ruby"];
+
+// for (const i in arr) {
+//   console.log(`index ${i} and value ${arr[i]}`);
+// }
+
+// const map = new Map();
+// map.set("IN", "India");
+// map.set("IN", "India");
+// map.set("USA", "America");
+// map.set("FR", "France");
+
+// Nothing happens
+// for (const i in map) {
+//   console.log(i);
+// }
