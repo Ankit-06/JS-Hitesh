124f99547fadd847029d7bf75a5e979641400d17
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2023-05-25 18:45:54 +0000

    map method & chaining

---
+++ 05Iterations/07_seven.js
@@ -0,0 +1,18 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// map method
+// const newNums = myNums.map((num) => num + 10);
+
+// const newNums = myNums.map((num) => {
+//   return num + 1;
+// });
+
+// console.log(newNums);
+
+// method chaining
+// const newNums = myNums
+//   .map((num) => num * 10)
+//   .map((num) => num + 5)
+//   .filter((num) => num > 50);
+
+// console.log(newNums);
