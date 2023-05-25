e646e1e8de9a22b35957ced41a7a561c07d7d787
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2023-05-25 17:33:25 +0000

    while & doWhile

---
+++ 05Iterations/02_two.js
@@ -0,0 +1,18 @@
+// let i = 1;
+// while (i <= 10) {
+//   console.log(`Value of i is ${i}`);
+//   i += 2;
+// }
+
+// const myArray = ["Flash", "Batman", "Superman"];
+// let index = 0;
+// while (index < myArray.length) {
+//   console.log(`Elements are ${myArray[index]}`);
+//   index++;
+// }
+
+let score = 11;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
