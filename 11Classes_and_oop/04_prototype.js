8d7804ef6a6a19ca74a8722a662c0175bfc823b1
Ankit Mishra <64202547+Ankit-06@users.noreply.github.com>
2023-07-27 20:16:41 +0000

    prototypal inheritance and __proto__

---
+++ 11Classes_and_oop/04_prototype.js
@@ -0,0 +1,65 @@
+// let myName = "Ankit     ";
+
+// console.log(myName.trueLength());
+
+let myHeros = ["thor", "spiderman"];
+
+let heroPower = {
+  thor: "hammer",
+  spiderman: "sling",
+
+  getSpiderPower: function () {
+    console.log(`Spidy power is ${this.spiderman}`);
+  },
+};
+
+Object.prototype.Ankit = function () {
+  console.log(`Ankit is present in all objects`);
+};
+
+heroPower.Ankit();
+myHeros.Ankit();
+
+Array.prototype.heyAnkit = function () {
+  console.log(`Ankit says hello`);
+};
+
+myHeros.heyAnkit();
+// heroPower.heyAnkit(); //error because function is set to only Array prototype.
+
+//Inheritance
+
+const User = {
+  userName: "Chai",
+  email: "chai@gmail.com",
+};
+
+const Teacher = {
+  makeVideo: true,
+};
+
+const TeachingSupport = {
+  isAvailable: false,
+};
+
+const TASupport = {
+  makeAssignment: "JS assignments",
+  fullTime: true,
+  __proto__: TeachingSupport,
+};
+
+Teacher.__proto__ = User; //outdated approch. User can be accessed now inside Teacher object(prototypal inheritance)
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher); //(prototypal inheritance)
+
+let anotherUserName = "chaiaurcode           ";
+String.prototype.trueLength = function () {
+  console.log(this);
+  console.log(this.name);
+  console.log(`true length is: ${this.trim().length()}`);
+};
+
+anotherUserName.trueLength();
+"ankit".trueLength();
+"iceTea".trueLength();
